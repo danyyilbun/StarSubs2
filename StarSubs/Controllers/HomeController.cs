96f1f5c01c489a90d5acd1ab1590e550996d894b
danyyilbun <danya.boon@gmail.com>
2019-10-01 19:52:17 -0400

    Redirect to new Controller Working

--- StarSubs/Controllers/HomeController.cs
+++ StarSubs/Controllers/HomeController.cs
@@ -11,6 +11,35 @@ namespace StarSubs.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            ViewBag.type = TempData["type"];
+            ViewBag.size = TempData["size"];
+            ViewBag.deal = TempData["deal"];
+
+            double dm;
+            Double.TryParse(TempData["mealDeal"].ToString(), out dm);
+            ViewBag.mealDeal = dm;
+
+            double pc;
+            Double.TryParse(TempData["Price"].ToString(), out pc);
+            ViewBag.Price = pc;
+
+            double cost;
+            Double.TryParse(TempData["Cost"].ToString(), out cost);
+            ViewBag.Cost = cost;
+
+
+            double tpc;
+            Double.TryParse(TempData["totalPrice"].ToString(), out tpc);
+            ViewBag.totalPrice = tpc;
+
+            double tx;
+            Double.TryParse(TempData["tax"].ToString(), out tx);
+            ViewBag.tax = tx;
+
+            double td;
+            Double.TryParse(TempData["totalDue"].ToString(), out td);
+            ViewBag.totalDue = td;
+
             return View();
         }
     }
--- StarSubs/Controllers/SubController.cs
+++ StarSubs/Controllers/SubController.cs
@@ -70,40 +70,8 @@ namespace StarSubs.Controllers
             Double.TryParse(TempData["totalPrice"].ToString(), out price);
 
             TempData["totalDue"] = (tx + price);
-            return RedirectToAction("Index3");
+            return RedirectToAction("Index","Home");
         }
-        public ActionResult Index3()
-        {
-            ViewBag.type = TempData["type"];
-            ViewBag.size = TempData["size"];
-            ViewBag.deal = TempData["deal"];
 
-            double dm;
-            Double.TryParse(TempData["mealDeal"].ToString(), out dm);
-            ViewBag.mealDeal = dm;
-
-            double pc;
-             Double.TryParse(TempData["Price"].ToString(), out pc);
-            ViewBag.Price = pc;
-
-            double cost;
-            Double.TryParse(TempData["Cost"].ToString(), out cost);
-            ViewBag.Cost = cost;
-
-
-            double tpc;
-            Double.TryParse(TempData["totalPrice"].ToString(), out tpc);
-            ViewBag.totalPrice = tpc;
-
-            double tx;
-            Double.TryParse(TempData["tax"].ToString(), out tx);
-            ViewBag.tax = tx;
-
-            double td;
-            Double.TryParse(TempData["totalDue"].ToString(), out td);
-            ViewBag.totalDue = td;
-
-            return View();
-        }
     }
 }
