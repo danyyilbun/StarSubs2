39775bc40c071e21acb5a7692a7f494bc3b6c112
danyyilbun <danya.boon@gmail.com>
2019-10-01 01:18:09 -0400

    Fully Working

--- StarSubs/Controllers/SubController.cs
+++ StarSubs/Controllers/SubController.cs
@@ -10,22 +10,56 @@ namespace StarSubs.Controllers
 {
     public class SubController : Controller
     {
+        int tax = 15;
+        Dictionary<string, double> type = new Dictionary<string, double>()
+        {
+            { " TheMichaelJackson", 1},{"ThePrince",2 },{"TheBackStreetBoys",3 },{"TheBeyonce",4 },{"TheMadonna",5 }
+
+        };
+        Dictionary<string, double> size = new Dictionary<string, double>()
+        {
+            { "OneHitWonder", 1 },{"BList",2 },{"AList",3 },{"Superstar",4}
+
+        };
+        Dictionary<string, double> deal = new Dictionary<string, double>()
+        {
+            { "None", 0 },{"DrinkAndChips",1.25 },{"DrinkAndCookies",2 }
+
+        };
         // GET: Sub
         public ActionResult Index()
         {
             var order = new OrderManipulation
             {
-                 Types = Enum.GetValues(typeof(SubType)).Cast<SubType>().Select(x => new SelectListItem { Text = x.ToString(), Value = x.ToString() }).AsEnumerable(),
+                 
+            Types = Enum.GetValues(typeof(SubType)).Cast<SubType>().Select(x => new SelectListItem { Text = x.ToString(), Value = x.ToString() }).AsEnumerable(),
                 Sizes = Enum.GetValues(typeof(SubSize)).Cast<SubSize>().Select(x => new SelectListItem { Text = x.ToString(), Value = x.ToString() }).AsEnumerable(),
                  Deals = Enum.GetValues(typeof(MealDeal)).Cast<MealDeal>().Select(x => x.ToString()).ToList()
             };
             return View(order);
         }
         [HttpPost]
-        public ActionResult Index(OrderManipulation order)
+        public ActionResult Index(OrderManipulation orderMan)
         {
+            var order = new OrderManipulation
+            {
 
-            return View();
+                Types = Enum.GetValues(typeof(SubType)).Cast<SubType>().Select(x => new SelectListItem { Text = x.ToString(), Value = x.ToString() }).AsEnumerable(),
+                Sizes = Enum.GetValues(typeof(SubSize)).Cast<SubSize>().Select(x => new SelectListItem { Text = x.ToString(), Value = x.ToString() }).AsEnumerable(),
+                Deals = Enum.GetValues(typeof(MealDeal)).Cast<MealDeal>().Select(x => x.ToString()).ToList()
+            };
+
+
+            ViewBag.type = orderMan.Type;
+            ViewBag.size = orderMan.Size;
+            ViewBag.deal = orderMan.Deal;
+            ViewBag.mealDeal = deal[orderMan.Deal];
+            ViewBag.Price = (type[orderMan.Type] * size[orderMan.Size]);
+            ViewBag.Cost = (type[orderMan.Type] * size[orderMan.Size]);
+            ViewBag.totalPrice = (type[orderMan.Type] * size[orderMan.Size]) + deal[orderMan.Deal];
+            ViewBag.tax = ViewBag.totalPrice * tax / 100;
+            ViewBag.totalDue = ViewBag.tax + ViewBag.totalPrice;
+            return View(order);
         }
 
     }
